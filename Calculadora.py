1a30d369cf53e3f0f59d799c2f962938c0b4d481
Abel Rovira <165647659+SACMM@users.noreply.github.com>
2024-10-27 00:01:57 +0200

    Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,15 +1,15 @@
 print("CALCULADORA")
-x = int(input("Primer numero: "))
-s = input("Operacion: ")
-y = int(input("Segundo numero: "))
-if s=="+" or s=="-" or s=="*" or s=="/":
-    if s == "+":
-        print("El resultado de",x,"+",y,"es igual a:" ,x + y)
+num1 = int(input("Primer numero: "))
+oper = input("Operacion: ")
+num2 = int(input("Segundo numero: "))
+if oper=="+" or oper=="-" or oper=="*" or oper=="/":
+    if oper == "+":
+        print("El resultado de la suma es igual a:" num1 + num2)
     elif s == "-":
-        print("El resultado de",x,"-",y,"es igual a:" ,x - y)
+        print("El resultado de la resta es igual a:" num1 - num2)
     elif s == "*":
-        print("El resultado de",x,"*",y,"es igual a:" ,x * y)
+        print("El resultado de la multiplicación es igual a:" num1 * num2)
     elif s == "/":
-        print("El resultado de",x,"/",y,"es igual a:" ,x / y)
+        print("El resultado de la division es igual a:" num1 / num2)
 else:
     print("Esta operacion no es valida")
