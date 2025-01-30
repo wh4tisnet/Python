7817c269c2171fac2009074d157b7a4b4fe07ba4
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:10:41 +0100

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,15 +1,19 @@
 print("CALCULADORA")
-num1 = int(input("Primer numero: "))
-oper = input("Operacion: ")
-num2 = int(input("Segundo numero: "))
-if oper=="+" or oper=="-" or oper=="*" or oper=="/":
+num1 = int(input("Primer número: "))
+oper = input("Operación (+, -, *, /): ")
+num2 = int(input("Segundo número: "))
+
+if oper in ["+", "-", "*", "/"]:
     if oper == "+":
-        print("El resultado de la suma es igual a:" num1 + num2)
-    elif s == "-":
-        print("El resultado de la resta es igual a:" num1 - num2)
-    elif s == "*":
-        print("El resultado de la multiplicación es igual a:" num1 * num2)
-    elif s == "/":
-        print("El resultado de la division es igual a:" num1 / num2)
+        print("El resultado de la suma es igual a:", num1 + num2)
+    elif oper == "-":
+        print("El resultado de la resta es igual a:", num1 - num2)
+    elif oper == "*":
+        print("El resultado de la multiplicación es igual a:", num1 * num2)
+    elif oper == "/":
+        if num2 != 0:
+            print("El resultado de la división es igual a:", num1 / num2)
+        else:
+            print("Error: División por cero no permitida.")
 else:
-    print("Esta operacion no es valida")
+    print("Esta operación no es válida")
