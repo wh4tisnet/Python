cb6e51e3a9485394fe7b81d77d3f2a21c8204112
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:27:26 +0100

    Delete pedidor de contraseñas en bucle.py

--- pedidor de contraseñas en bucle.py
+++
@@ -1,8 +0,0 @@
-print("Introduce tu contraseña")
-contra=input("")
-while contra!="informatica":
-    print("Contraseña incorrecta")
-    contra=input("Contraseña: ")
-print("Contraseña correcta")
-    
-    
