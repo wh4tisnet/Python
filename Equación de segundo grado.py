13f07c612df6c1667eca2549a0c4ccded85dc3c9
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:13:35 +0100

    Update Equación de segundo grado.py

--- Equación de segundo grado.py
+++ Equación de segundo grado.py
@@ -1,13 +1,16 @@
 import math
-print("Equación de segundo grado")
+
+print("Ecuación de segundo grado")
 a = float(input('Dame el valor del coeficiente a: '))
 b = float(input('Dame el valor del coeficiente b: '))
 c = float(input('Dame el valor del coeficiente c: '))
-d = b*b-4*a*c
-if d <0:
-    print("Esta equacio nose")
+
+d = b**2 - 4*a*c
+
+if d < 0:
+    print("Esta ecuación no tiene soluciones reales.")
 else:
-    x1 = (-b+math.sqrt(d))/2*a
-    print("El primer numero: x1 = " + str(x1))
-    x2 = (-b-math.sqrt(d))/2*a
-    print("El segundo numero: x2 = " + str(x2))
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    print(f"El primer número: x1 = {x1}")
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    print(f"El segundo número: x2 = {x2}")
