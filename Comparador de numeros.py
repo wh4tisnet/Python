d7d12053716d34c9f9dbf2fc75b7e59c094ebaa7
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:12:19 +0100

    Update Comparador de numeros.py

--- Comparador de numeros.py
+++ Comparador de numeros.py
@@ -1,10 +1,11 @@
-print("Introduce el primer numero: ")
-num1=input("")
-print("Introduce el segundo numero: ")
-num2=input("")
-if num1<num2:
-    print("El numero 2 es mayor que el numero 1")
-elif num1>num2:
-    print("El numero 1 es mayor que el numero 2")
-elif num1==num2:
-    print("Los numero son iguales")
+print("Introduce el primer número: ")
+num1 = int(input())
+print("Introduce el segundo número: ")
+num2 = int(input())
+
+if num1 < num2:
+    print("El número 2 es mayor que el número 1")
+elif num1 > num2:
+    print("El número 1 es mayor que el número 2")
+else:
+    print("Los números son iguales")
