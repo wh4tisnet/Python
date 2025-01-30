e9e1a36f31deeaa1321bf4509eb1aec49d22c6a5
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:17:06 +0100

    Update and rename Mat Beta.py to lista_tarea.py

---
+++ lista_tarea.py
@@ -0,0 +1,29 @@
+nombre = input("Por favor introduzca su nombre: ")
+print(f"Es un gusto, {nombre}")
+print("Déjame presentarme, yo soy Mat y soy un mini programa informático que te ayudará con tus tareas a lo largo del día.")
+
+tareas = []
+
+# Solicitar las primeras 6 tareas
+for i in range(1, 7):
+    tarea = input(f"Por favor dime la tarea {i}: ")
+    tareas.append(tarea)
+
+print("Entendido señor, sus tareas son:", ", ".join(tareas))
+
+# Solicitar más tareas si es necesario
+while True:
+    continuar = input("¿Quiere añadir más tareas? (s para sí, n para no): ").lower()
+    if continuar == 'n':
+        break
+    tarea = input("Por favor, introduzca la nueva tarea: ")
+    tareas.append(tarea)
+
+print("Ahora su lista completa de tareas es:", ", ".join(tareas))
+print("Señor, ¿está seguro de que no necesita nada más?")
+print("Estoy aquí para servirle señor, si necesita algo más no dude en solicitar mi ayuda.")
+
+with open("lista_tareas.txt", "w") as file:
+    file.write("\n".join(tareas))
+
+print("Su lista de tareas ha sido guardada en 'lista_tareas.txt'.")
