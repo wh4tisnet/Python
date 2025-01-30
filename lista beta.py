0f33b34de2a473710d30c399e9ddf8c4ab84e47e
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:25:49 +0100

    Delete lista beta.py

--- lista beta.py
+++
@@ -1,6 +0,0 @@
-print("========================================")
-print("*** Programa que analiza una palabra ***")
-print("========================================")
-pal=input("Introduce una palabra: ")
-list[pal]
-print(list[1:3])
