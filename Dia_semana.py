372d6fffb1ecbe08ec092f8c881ed8b55cb56a46
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:25:33 +0100

    Update and rename diasSemanas.py to Dia_semana.py

---
+++ Dia_semana.py
@@ -0,0 +1,4 @@
+dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+
+for dia in dias_semana:
+    print(f"El día de la semana es: {dia} y tiene {len(dia)} letras")
--- diasSemanas.py
+++
@@ -1,4 +0,0 @@
-diaSemana=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
-for dia in diaSemana:
-    print("El dia de la semana es: ",dia,"i tiene",len(dia),"letras")
-
